84a96189093afb6548fe3d83f7c3865518646df5
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-20 13:47:59 -0400

    Add gitignore file and ability to save data to text file

--- functions.py
+++ functions.py
@@ -18,12 +18,39 @@ def add_student(name, student_id=332):
     students.append(student)
 
 
-userAnswer = input("Would you like to enter a student? ")
-while userAnswer == "yes":
-    student_name = input("Enter students name: ")
-    student_id = input("Enter student Id:")
+def save_file(student):
+    try:
+        file = open("students.txt", "a")
+        file.write(student + "\n")
+        file.close()
+    except Exception:
+        print("Could not save file")
 
-    add_student(student_name, student_id)
-    userAnswer = input("Would you like to continue entering students? ")
 
+def read_file():
+    try:
+        file = open("students.txt", "r")
+        for student in file.readlines():
+            add_student(student)
+        file.close()
+    except Exception:
+        print("Could not read file")
+
+
+read_file()
 print_students_titlecase()
+
+student_name = input("Enter students name: ")
+student_id = input("Enter student Id:")
+
+save_file(student_name)
+
+# userAnswer = input("Would you like to enter a student? ")
+# while userAnswer == "yes":
+#     student_name = input("Enter students name: ")
+#     student_id = input("Enter student Id:")
+#
+#     add_student(student_name, student_id)
+#     userAnswer = input("Would you like to continue entering students? ")
+#
+# print_students_titlecase()
