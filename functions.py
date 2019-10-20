6811e16d4f0326e1a9dc39aef8dfdc54cd85e8b9
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-19 21:27:11 -0400

    initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+students = []
+
+
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student.title()
+    return students_titlecase
+
+
+def print_students_titlecase():
+    students_titlecase = get_students_titlecase()
+    print(students_titlecase)
+
+
+def add_student(name):
+    students.append(name)
+
+
+print_students_titlecase()
