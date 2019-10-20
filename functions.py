d0f57d5d1fac39af7509b984a8c438a08762399c
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-20 13:37:40 -0400

    Add user request for informtion

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 
@@ -18,8 +18,12 @@ def add_student(name, student_id=332):
     students.append(student)
 
 
-student_name = input("Enter students name: ")
-student_id = input("Enter student Id:")
+userAnswer = input("Would you like to enter a student? ")
+while userAnswer == "yes":
+    student_name = input("Enter students name: ")
+    student_id = input("Enter student Id:")
 
-add_student(student_name, student_id)
+    add_student(student_name, student_id)
+    userAnswer = input("Would you like to continue entering students? ")
+
 print_students_titlecase()
