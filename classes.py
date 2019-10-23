016dcebf3a42a48d437bf59d2b85cb17d64f6c2f
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-22 21:18:31 -0400

    Update student class to have a static school name

--- classes.py
+++ classes.py
@@ -2,19 +2,27 @@ students = []
 
 
 class Student:
+
+    school_name = "Georgia Institute of Technology"
+
     def __init__(self, name, student_id=42):
         self.name = name
         self.student_id = student_id
         students.append(self)
 
     def __str__(self):
-        return "Student"
+        return "Student " + self.name
 
     def get_name_capitalize(self):
         return self.name.capitalize()
+
+    def get_school_name(self):
+        return self.school_name
 
 
 bob = Student("Bob")
 
 print(bob)
 print(students)
+
+print(Student.school_name)
