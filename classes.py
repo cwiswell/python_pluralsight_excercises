acceba815bd96d5777f7156e2b7e24e4fea1dc48
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-22 21:14:39 -0400

    Update student class to use class fields

--- classes.py
+++ classes.py
@@ -3,11 +3,15 @@ students = []
 
 class Student:
     def __init__(self, name, student_id=42):
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
 
     def __str__(self):
         return "Student"
+
+    def get_name_capitalize(self):
+        return self.name.capitalize()
 
 
 bob = Student("Bob")
