be880c4b306c54979f250a693530e298b5658c1a
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-24 20:01:02 -0400

    Add test webapp using flask package

---
+++ test_webapp.py
@@ -0,0 +1,11 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+# need to run in CMD: set FLASK_APP=test_webapp.py
+# then run: python -m flask run
