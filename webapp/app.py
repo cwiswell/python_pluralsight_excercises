09c950a60e9f0ddbdb0ae86e29abbf33eb82805a
Craig A Wiswell <cwiswell63@gmail.com>
2019-10-25 19:36:29 -0400

    Separate post and get methods for student page

--- webapp/app.py
+++ webapp/app.py
@@ -7,19 +7,22 @@ students = []
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def students_page():
-    if request.method == "POST":
-        new_student_id = request.form.get("student-id", "")
-        new_student_name = request.form.get("name", "")
-        new_student_last_name = request.form.get("last-name", "")
-
-        new_student = Student(name=new_student_name, last_name=new_student_last_name, student_id=new_student_id)
-        students.append(new_student)
-
-        return redirect(url_for("students_page"))
-
     return render_template("index.html", students=students)
+
+
+@app.route("/", methods=["POST"])
+def add_student():
+    new_student_id = request.form.get("student-id", "")
+    new_student_name = request.form.get("name", "")
+    new_student_last_name = request.form.get("last-name", "")
+
+    new_student = Student(name=new_student_name, last_name=new_student_last_name, student_id=new_student_id)
+    students.append(new_student)
+
+    return redirect(url_for("students_page"))
+
 
 # need to run in CMD: set FLASK_APP=app.py
 # set in CMD: FLASK_ENV=development
