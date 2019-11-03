44492da07899ee86340ee4a908087db188e92911
Craig A Wiswell <cwiswell63@gmail.com>
2019-11-03 01:11:09 -0400

    Move logic into separate method

--- webapp/app.py
+++ webapp/app.py
@@ -14,15 +14,18 @@ def students_page():
 
 @app.route("/", methods=["POST"])
 def add_student():
-    new_student_id = request.form.get("student-id", "")
-    new_student_name = request.form.get("name", "")
-    new_student_last_name = request.form.get("last-name", "")
-
-    new_student = Student(name=new_student_name, last_name=new_student_last_name, student_id=new_student_id)
-    students.append(new_student)
+    process_add_request(request.form)
 
     return redirect(url_for("students_page"))
 
+
+def process_add_request(form):
+    new_student_id = form.get("student-id", "")
+    new_student_name = form.get("name", "")
+    new_student_last_name = form.get("last-name", "")
+
+    new_student = Student(name=new_student_name, last_name=new_student_last_name, student_id=new_student_id)
+    students.append(new_student)
 
 # need to run in CMD: set FLASK_APP=app.py
 # set in CMD: FLASK_ENV=development
