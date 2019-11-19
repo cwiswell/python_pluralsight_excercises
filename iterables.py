69d896909ccc5a46c7ca78bcfe21fa7f9f19fef6
Craig A Wiswell <cwiswell63@gmail.com>
2019-11-18 22:01:33 -0500

    Add is_prime filtering predicate

--- iterables.py
+++ iterables.py
@@ -1,5 +1,25 @@
+from math import sqrt
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+primes = [x for x in range(101) if is_prime(x)]
+
+print(primes)
+
+
 words = "Some sentences have lots of words and some have little".split()
 
 wordLengths = [len(word) for word in words]
+
+
+
+
 
 print(wordLengths)
