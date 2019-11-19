4507de5c107484b8168674e41feba542163fa2e9
Craig A Wiswell <cwiswell63@gmail.com>
2019-11-19 17:07:05 -0500

    Add block of code to test next on iter

--- iterables.py
+++ iterables.py
@@ -1,5 +1,6 @@
 from math import sqrt
 
+
 def is_prime(x):
     if x < 2:
         return False
@@ -13,13 +14,19 @@ primes = [x for x in range(101) if is_prime(x)]
 
 print(primes)
 
-
 words = "Some sentences have lots of words and some have little".split()
 
 wordLengths = [len(word) for word in words]
 
-
-
-
-
 print(wordLengths)
+
+
+"""Block to test use of next on an iterator"""
+seasons = ['Spring', 'Summer', 'Autumn', 'Winter']
+
+iterator = iter(seasons)
+
+print(next(iterator))
+
+print(next(iterator))
+
